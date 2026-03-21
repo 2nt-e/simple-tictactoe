1c7c627c7ce69356314c23083e331685d7eac16e
2nt_ <85730229+2nt-e@users.noreply.github.com>
2026-03-21 17:11:13 +0530

    changed name of a file.

---
+++ main.py
@@ -0,0 +1,133 @@
+from better_print import *
+
+class Game:
+    def __init__(self):
+        self.map = {
+            "1" : "1️⃣",
+            "2" : "2️⃣",
+            "3" : "3️⃣",
+            "4" : "4️⃣",
+            "5" : "5️⃣",
+            "6" : "6️⃣",
+            "7" : "7️⃣",
+            "8" : "8️⃣",
+            "9" : "9️⃣",
+            }
+        self.turn = 1
+        self.ocupation = []
+
+    def print(self): # For printing structure
+                for _ in range(1, 10):
+                    if _ % 3 == 0:
+                        better.print(self.map[f"{_}"], end="\n")
+                    else:
+                        better.print(self.map[f"{_}"], end=" ")
+    
+    def check_chance(self): # checking for chance
+        if turn%2 == 0: 
+            turn += 1
+            return "#️⃣" 
+        else: 
+            turn += 1
+            return "⏺️"
+    
+    def contraditon(self, change):
+        try: 
+            if change not in self.ocupation:
+                return False
+            else:
+                return "already_occupied"
+        except:
+            return "invalid_input"
+    
+    def game_end(self):
+        ...
+
+
+
+
+
+def game():
+    better.print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")  # Clear the console for better visibility
+    while game_end() == False: # if game is still running.
+
+        global turn, ocupation, default_structure
+        print("\n")
+        print_structure() # printing structre.
+
+        cc = chance_check() # checking whose chance this is.
+        change = better.input(f"Where to put {cc} : ").strip() # takling input fomr that player.
+        contra = contraditon(change= change) # checking for contraditon
+
+        if contra == False: # if no contraditon.
+            default_structure[change] = cc # editing default structue.
+            ocupation.append(int(change)) # adding occupation.
+            turn = turn + 1 # incereaseing value for turn.
+        # for contraditon.
+        elif contra == "o": #
+            better.print("That Place is already occupied!")
+        elif contra == "uv" or "e":
+            better.print("Please Enter a Valid Value!")
+        # contraditon ends.
+
+
+    if game_end() == "tie": # telling tie.
+        better.print("")
+        print_structure()
+        better.print("Game Ended with a Tie!")
+    elif game_end() == "hw": # telling hash won.
+        better.print("")
+        print_structure()
+        better.print("⏺️ lost the game!") 
+    elif game_end() == "cw": # telling circle won.
+        better.print("")
+        print_structure()
+        better.print("#️⃣ lost the game!")
+    else: # if something went wrong
+        better.print("")
+        print_structure()
+        better.print("something wrong went!")
+
+
+def game_end(): # function to check if game should still be running and how it will end.
+    global default_structure, ocupation 
+
+# all conditon for either hash or circle to win the game.
+
+    r1h = ("#️⃣" ==  default_structure["1"] == default_structure["2"] == default_structure["3"])
+    r2h = ("#️⃣" ==  default_structure["4"] == default_structure["5"] == default_structure["6"])
+    r3h = ("#️⃣" ==  default_structure["7"] == default_structure["8"] == default_structure["9"])
+
+    c1h = ("#️⃣" ==  default_structure["1"] == default_structure["4"] == default_structure["7"])
+    c2h = ("#️⃣" ==  default_structure["2"] == default_structure["5"] == default_structure["8"])
+    c3h = ("#️⃣" ==  default_structure["3"] == default_structure["6"] == default_structure["9"])
+
+    d1h = ("#️⃣" ==  default_structure["1"] == default_structure["5"] == default_structure["9"])
+    d2h = ("#️⃣" ==  default_structure["3"] == default_structure["5"] == default_structure["7"])
+
+
+    r1c = ("⏺️" ==  default_structure["1"] == default_structure["2"] == default_structure["3"])
+    r2c = ("⏺️" ==  default_structure["4"] == default_structure["5"] == default_structure["6"])
+    r3c = ("⏺️" ==  default_structure["7"] == default_structure["8"] == default_structure["9"])
+
+    c1c = ("⏺️" ==  default_structure["1"] == default_structure["4"] == default_structure["7"])
+    c2c = ("⏺️" ==  default_structure["2"] == default_structure["5"] == default_structure["8"])
+    c3c = ("⏺️" ==  default_structure["3"] == default_structure["6"] == default_structure["9"])
+
+    d1c = ("⏺️" ==  default_structure["1"] == default_structure["5"] == default_structure["9"])
+    d2c = ("⏺️" ==  default_structure["3"] == default_structure["5"] == default_structure["7"])
+
+# condition ends.
+ 
+    if r1h or r2h or r3h or c1h or c2h or c3h or d1h or d2h == True: # checking for hash's win.
+        return "hw" 
+    elif r1c or r2c or r3c or c1c or c2c or c3c or d1c or d2c == True: # checking for circle's win
+        return "cw" 
+    elif chance_check() == None: # checking if any free space is avalable to fill.
+        return "tie"
+    else:  # if Game has not ended yet.
+        return False
+        
+
+
+game()
